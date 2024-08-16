d89a43c6daae44f30bf2fd7d5ebe3ef11f9fb36e
chie-hira <capella.a36@gmail.com>
2024-08-16 17:17:13 +0900

    データベース接続

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const tasksRouter = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+app.use(express.json()) // JSONデータを受け取るためのミドルウェア
 
 // ルーティング設計
 app.use('/api/v1/tasks', tasksRouter)
 
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`)
-} )
+// データベース接続
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => {
+      console.log(`App is running on http://localhost:${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
