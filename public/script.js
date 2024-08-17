8a2033586975d22e4fb72cb3f111f5601ea08d26
chie-hira <capella.a36@gmail.com>
2024-08-18 00:07:44 +0900

    エラーハンドリング

--- public/script.js
+++ public/script.js
@@ -1,22 +1,23 @@
-const tasksDOM = document.querySelector(".tasks")
-const formDOM = document.querySelector(".task-form")
-const taskInputDOM = document.querySelector(".task-input")
+const tasksDOM = document.querySelector(".tasks");
+const formDOM = document.querySelector(".task-form");
+const taskInputDOM = document.querySelector(".task-input");
+const formAlertDOM = document.querySelector(".form-alert");
 
 // タスクを表示する関数
 const showTasks = async () => {
     try {
         // const tasks = await axios.get('/api/v1/tasks')
         const { data: tasks } = await axios.get("/api/v1/tasks");
-        // console.log(tasks);
 
         // タスクがない場合
         if (tasks.length === 0) {
-            tasksDOM.innerHTML = `<h5 class="empty-list">タスクがありません</h5>`
-            return
+            tasksDOM.innerHTML = `<h5 class="empty-list">タスクがありません</h5>`;
+            return;
         }
 
-        const allTasks = tasks.map((task) => {
-                const { _id, name, description } = task // 分割代入、最近の書き方
+        const allTasks = tasks
+            .map((task) => {
+                const { _id, name, description } = task; // 分割代入、最近の書き方
                 // console.log(name)
                 // console.log(task.name); この方法もある
 
@@ -37,48 +38,75 @@ const showTasks = async () => {
                       </div>
                   </div>
                   `;
-            }).join("")
+            })
+            .join("");
 
-            tasksDOM.innerHTML = allTasks
+        tasksDOM.innerHTML = allTasks;
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
 
-showTasks()
+showTasks();
 
 // タスクを追加する処理
 formDOM.addEventListener("submit", async (e) => {
-    e.preventDefault() // ページのリロードを防ぐ
+    e.preventDefault(); // ページのリロードを防ぐ
 
-    const name = taskInputDOM.value
+    const name = taskInputDOM.value;
+
+    if (name.length > 20) {
+        displayMessage("文字数が多すぎます", "text-alert");
+
+        return;
+    }
 
     try {
         await axios.post("/api/v1/tasks", {
             // name: name
-            name // 省略形
-        })
+            name, // 省略形
+        });
 
-        taskInputDOM.value = ""
-        showTasks()
+        showTasks();
+        taskInputDOM.value = "";
+        displayMessage("タスクを追加しました", "text-success");
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        displayMessage("エラーが発生しました", "text-alert");
     }
-} )
+});
 
 // タスクを削除する処理
 tasksDOM.addEventListener("click", async (e) => {
-    const parentElement = e.target.parentElement
+    const parentElement = e.target.parentElement;
     // console.log(element.parentElement);
-    
+
     if (parentElement.classList.contains("delete-btn")) {
-        const taskId = parentElement.dataset.id
+        const taskId = parentElement.dataset.id;
 
         try {
-            await axios.delete(`/api/v1/tasks/${taskId}`)
-            showTasks()
+            await axios.delete(`/api/v1/tasks/${taskId}`);
+            showTasks();
+            displayMessage("タスクを削除しました", "text-success");
         } catch (error) {
-            console.log(error)
+            console.log(error);
+            displayMessage("エラーが発生しました", "text-alert");
         }
     }
-} )
+});
+
+const hiddenMessage = () => {
+    setTimeout(() => {
+        formAlertDOM.style.display = "none";
+        formAlertDOM.classList.remove("text-success");
+        formAlertDOM.classList.remove("text-alert");
+    }, 3000); // 3秒後にエラーメッセージを消す
+};
+
+const displayMessage = (message, addClass) => {
+    formAlertDOM.classList.add(addClass); // クラスを追加して文字色を変える
+    formAlertDOM.textContent = message;
+    formAlertDOM.style.display = "block";
+
+    hiddenMessage();
+};
